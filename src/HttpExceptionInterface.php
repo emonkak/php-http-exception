0ae024e7fefbecd79df4033111b58aeb022adc2b
Shota Nozaki <emonkak@gmail.com>
2020-02-17 08:48:04 +0900

    HttpExceptionInterface: Fix doc types

--- src/HttpExceptionInterface.php
+++ src/HttpExceptionInterface.php
@@ -9,14 +9,14 @@ interface HttpExceptionInterface extends \Throwable
     /**
      * Returns the status code.
      *
-     * @return An HTTP response status code
+     * @return int An HTTP response status code
      */
     public function getStatusCode(): int;
 
     /**
      * Returns response headers.
      *
-     * @return Response headers
+     * @return array<string,string|string[]> Response headers
      */
     public function getHeaders(): array;
 }
